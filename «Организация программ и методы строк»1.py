0b96a3cb12906c62a220d50e86a49ed108845ed7
TIR1991 <rildal2000@gmail.com>
2024-10-17 18:41:08 +0300

    Add files via upload

---
+++ «Организация программ и методы строк»1.py
@@ -0,0 +1,7 @@
+my_string = 'моя семья'
+name = input(my_string)
+print(my_string .upper())
+print(my_string .upper().lower())
+print(my_string .replace(' ',''))
+print(my_string .replace('семья',''))
+print(my_string .replace('моя',''))
